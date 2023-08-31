c42ee1534fb095a444f55965b35827c464a8fe53
leandrocoutinho1 <leandro.coutinho@loft.com.br>
2023-08-31 16:15:56 -0300

    Arquivo 1118

--- uri-1118.c
+++ uri-1118.c
@@ -5,19 +5,36 @@ int main() {
     int continua = 1;
     
     while(continua == 1){
-        scanf("%f", &N1);
-        while(N1 < 0 || N1 > 10){
-            printf("nota invalida\n");
+
+        do
+        {
             scanf("%f", &N1);
-        }
-        scanf("%f", &N1);
-        while(N2 < 0 || N2 > 10){
-            printf("nota invalida\n");
+            if(N1 < 0 || N1 > 10){
+                printf("nota invalida\n");
+            }
+
+        } while (N1 < 0 || N1 > 10);
+
+        do
+        {
             scanf("%f", &N2);
-        }
+            if(N2 < 0 || N2 > 10){
+                printf("nota invalida\n");
+            }
+
+        } while (N2 < 0 || N2 > 10);
+
         
-        printf("media = %.2f\n", (N1 + N2)/ 2)
+        printf("media = %.2f\n", (N1 + N2)/ 2);
         
+
+
+        do
+        {
+            printf("novo calculo (1-sim 2-nao)\n");
+            scanf("%d", &continua);
+
+        } while (continua < 1 || continua > 2);
         
     }
     
