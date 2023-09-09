857150af1900808e467c2463e83c8c253ba66f90
leandrocoutinho1 <leandro.coutinho@loft.com.br>
2023-09-08 21:29:37 -0300

    Exercício 2 Struct

--- struct_2.c
+++ struct_2.c
@@ -7,26 +7,38 @@ struct Data {
 };
 
 int main() {
+    int maior = 0;
     
     struct Data datas[3];
 
     for (int i = 0; i < 3; i++){
         printf("Digite o dia da data %d: ", i + 1);
         scanf("%d", &datas[i].dia);
+        
 
         printf("Digite o mes da data %d: ", i + 1);
         scanf("%d", &datas[i].mes);
+        
 
         printf("Digite o ano da data %d: ", i + 1);
-        scanf("%d", &datas[i].anzo);
+        scanf("%d", &datas[i].ano);
+        printf("\n");
 
     }
 
     printf("Datas cadastradas: \n");
 
     for (int i = 0; i < 3; i++){
-        printf("Telefone %d: (%d) %ld. \n", i+ 1, telefones[i].ddd, telefones[i].telefone);
+        printf("Data %d: %d/%d/%d. \n", i+ 1, datas[i].dia, datas[i].mes, datas[i].ano);
     }
+
+    for (int i = 0; i < 3; i++){
+        if (datas[i].ano > maior){
+            maior = datas[i].ano;
+        }
+    }
+
+    printf("Maior ano: %d. \n", maior);
 
     return 0;
 }
